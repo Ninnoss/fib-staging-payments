68163ddaa9c71fa71ba38bbde4d1e069f8187188
Ninos <ninos.dinkha97@gmail.com>
2024-06-23 13:25:08 +0300

    update payment status skeleton

--- src/components/PaymentStatus.jsx
+++ src/components/PaymentStatus.jsx
@@ -1,25 +1,32 @@
 /* eslint-disable react/prop-types */
-import LoadingSpinner from './LoadingSpinner';
-
 const PaymentStatus = ({ paymentStatus, isLoading }) => {
   return (
     <div className="flex flex-col mt-4">
-      {isLoading && <LoadingSpinner />}
-      {!isLoading && paymentStatus && (
+      {isLoading ? (
         <>
-          <p>Status: {paymentStatus?.status}</p>
-          {paymentStatus?.status === 'PAID' && (
-            <>
-              <p>Paid At: {new Date(paymentStatus.paidAt).toLocaleString()}</p>
-              <p>Paid By: {paymentStatus?.paidBy.name}</p>
-              <p>IBAN: {paymentStatus?.paidBy.iban}</p>
-            </>
-          )}
-          {paymentStatus?.status === 'UNPAID' && (
-            <p>Description: The transaction is not paid yet and is still pending.</p>
-          )}
-          {paymentStatus?.status === 'DECLINED' && <p>Description: Payment has been declined.</p>}
+          <div className="animate-pulse flex flex-col space-y-4">
+            <div className="h-4 bg-gray-200 rounded w-3/4"></div>
+            <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+            <div className="h-4 bg-gray-200 rounded w-1/4"></div>
+          </div>
         </>
+      ) : (
+        paymentStatus && (
+          <>
+            <p>Status: {paymentStatus?.status}</p>
+            {paymentStatus?.status === 'PAID' && (
+              <>
+                <p>Paid At: {new Date(paymentStatus.paidAt).toLocaleString()}</p>
+                <p>Paid By: {paymentStatus?.paidBy.name}</p>
+                <p>IBAN: {paymentStatus?.paidBy.iban}</p>
+              </>
+            )}
+            {paymentStatus?.status === 'UNPAID' && (
+              <p>Description: The transaction is not paid yet and is still pending.</p>
+            )}
+            {paymentStatus?.status === 'DECLINED' && <p>Description: Payment has been declined.</p>}
+          </>
+        )
       )}
     </div>
   );
