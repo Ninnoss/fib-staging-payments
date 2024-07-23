e63e3fac467e1e8cc40229a11b5cfa44b21f8751
Ninos <ninos.dinkha97@gmail.com>
2024-07-23 11:03:42 +0300

    added hook to exchange tokens

---
+++ src/hooks/useAuthCodeExchange.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useAuthCodeExchange = () => {
+  const location = useLocation();
+
+  const [tokenData, setTokenData] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const authorizationCode = queryParams.get('code');
+
+    if (authorizationCode) {
+      exchangeAuthorizationCodeForTokens(authorizationCode);
+    }
+  }, [location.search]);
+
+  const exchangeAuthorizationCodeForTokens = async (authorizationCode) => {
+    const redirectUri = 'http://localhost:3000';
+
+    const tokenEndpoint =
+      'https://fib.stage.fib.iq/auth/realms/fib-personal-application/protocol/openid-connect/token';
+    const params = new URLSearchParams({
+      client_id: import.meta.env.VITE_FIB_SSO_CLIENT_ID,
+      client_secret: import.meta.env.VITE_FIB_SSO_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code: authorizationCode,
+      redirect_uri: redirectUri,
+    });
+
+    try {
+      const response = await fetch(tokenEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: params.toString(),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to exchange authorization code for tokens');
+      }
+
+      const data = await response.json();
+      setTokenData(data);
+      fetchUserData(data.access_token);
+    } catch (error) {
+      setError(error);
+      console.error('Error exchanging authorization code for tokens:', error);
+    }
+  };
+
+  const fetchUserData = async (accessToken) => {
+    const userInfoEndpoint = 'https://fib.stage.fib.iq/protected/v1/sso-user-details';
+
+    try {
+      const response = await fetch(userInfoEndpoint, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch user details');
+      }
+
+      const userData = await response.json();
+      setUserData(userData);
+    } catch (error) {
+      setError(error);
+      console.error('Error fetching user details:', error);
+    }
+  };
+
+  return { tokenData, userData, error };
+};
+
+export default useAuthCodeExchange;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import LoginWithFIB from '../components/LoginWithFIB';
+import useAuthCodeExchange from '../hooks/useAuthCodeExchange';
 
 const Home = () => {
+  const { tokenData, error } = useAuthCodeExchange();
+
+  if (tokenData) {
+    return (
+      <div>
+        <h1>Tokens</h1>
+        <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+      </div>
+    );
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <main className="flex flex-col md:flex-row flex-wrap items-center justify-center gap-6 py-16 md:py-28 lg:py-64">
       <Link to="/create-payment">
