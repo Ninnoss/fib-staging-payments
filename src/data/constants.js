725507e40f36603536501c1809b349af00254603
Ninos <ninos.dinkha97@gmail.com>
2024-07-25 13:24:31 +0300

    add vite proxy server

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,6 +7,6 @@ export const AUTH_TOKEN_URL =
 export const SSO_BASE_URL =
   'https://fib.stage.fib.iq/auth/realms/fib-personal-application/protocol/openid-connect/auth';
 
-export const SSO_USER_DETAILS_URL = 'https://fib.stage.fib.iq/protected/v1/sso-user-details';
-
-export const PAYMENTS_BASE_URL = 'https://fib.stage.fib.iq/protected/v1/payments';
+export const API_BASE_URL = '/api/protected/v1';
+export const SSO_USER_DETAILS_URL = `${API_BASE_URL}/sso-user-details`;
+export const PAYMENTS_BASE_URL = `${API_BASE_URL}/payments`;
